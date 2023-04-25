c6f4d7354be97c5ede45eb493492b740abf14ccc
RAZA11-A <95311689+RAZA11-A@users.noreply.github.com>
2023-04-25 06:06:52 -0700

    Add files via upload

---
+++ scannerClass.java
@@ -0,0 +1,17 @@
+package Raza;
+import java.util.Scanner;
+public class scannerClass {
+
+	public static void main(String[] args) {
+
+System.out.println("enter any two no");
+Scanner sc = new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+
+System.out.println("Addition of two no "+(a+b));
+	
+	
+	}
+	
+}
